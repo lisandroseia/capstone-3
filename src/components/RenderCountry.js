539695021684c465486709948d9082c68f10ba5e
lisandroseia <lisandroseia11@gmail.com>
2022-03-10 12:11:30 -0300

    Add testing for reducer

--- src/components/RenderCountry.js
+++ src/components/RenderCountry.js
@@ -2,18 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/country.scss';
 
-const RenderCountry = ({ name, active, total }) => {
+const RenderCountry = ({ name, active }) => {
   return (
     <li className="list-wraper">
       <Link to={`/details/${name}`}>
         {
           <div>
             <h2>{name}</h2>
-            <p className='active'>
+            <p className="active">
               active covid cases: <span>{active}</span>
-            </p>
-            <p className='total'>
-              total covid cases: <span>{total}</span>
             </p>
           </div>
         }
